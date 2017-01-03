9eefe4f9cf65c3f12654b26edb549b7c681a2dce
Jonathan Adler <jonadler@users.noreply.github.com>
2017-01-02 17:29:22 -0800

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,44 @@
+﻿namespace TweetImage
+
+type Config = {
+    TweetImageUrl: string
+    TweetText: string
+    ConsumerKey: string
+    ConsumerSecret: string
+    AccessToken: string
+    AccessTokenSecret: string
+}
+
+module Program =
+    open System
+    open System.IO
+    open System.Text
+    open Amazon.Lambda.Core
+    open Tweetinvi.Core
+    open Newtonsoft.Json
+    
+    let getConfig (filename:string) =
+        File.ReadAllText filename
+        |> JsonConvert.DeserializeObject<Config>
+
+    let getImage (url:string) =
+        let client = new System.Net.Http.HttpClient()
+        client.GetByteArrayAsync(url)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    let handler(context:ILambdaContext) =
+        let config = getConfig "config.json"
+
+        Tweetinvi.Auth.ApplicationCredentials <- 
+            Tweetinvi.Auth.CreateCredentials(
+                config.ConsumerKey,
+                config.ConsumerSecret,
+                config.AccessToken,
+                config.AccessTokenSecret)
+        
+        let image = getImage config.TweetImageUrl
+        let text = config.TweetText
+
+        Tweetinvi.Tweet.PublishTweetWithImage(text,image)
+        |> ignore
